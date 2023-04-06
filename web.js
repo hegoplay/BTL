af4e8f08b669975a791c102b856bea2bc6adb5f7
quangG01 <quydang1404@gmail.com>
2023-04-06 15:47:05 +0700

    Loading slider

---
+++ web.js
@@ -0,0 +1,49 @@
+var images = document.querySelectorAll(".slide_img");
+var prevBtn = document.querySelector(".slide_box .controls #prevBtn");
+var nextBtn = document.querySelector(".slide_box .controls #nextBtn");
+var currentIndex = 0;
+var intervalID;
+
+function showImage(index) {
+    if(index < 0){
+        index = images.length -1;
+    }
+    else if (index >= images.length)
+    {
+        index = 0;
+    }
+    images[currentIndex].classList.remove("active");
+    images[index].classList.add("active");
+    currentIndex = index;
+}
+
+function showNext() {
+    showImage(currentIndex + 1);
+}
+
+function showPrevious() {
+    showImage(currentIndex - 1);
+}
+
+function startSlider() {
+    intervalID = setInterval(showNext, 3000);
+}
+
+function stopSlider() {
+    clearInterval(intervalID);
+}
+
+prevBtn.addEventListener("click", function() {
+    // console.log(123);
+    stopSlider();
+    showPrevious();
+});
+
+nextBtn.addEventListener("click", function() {
+    // console.log(456);
+    
+    stopSlider();
+    showNext();
+});
+
+startSlider();
